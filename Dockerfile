61e9ede83927cff9845766871b49c8626dd0097a
Joe Petrini <joepetrini@gmail.com>
2018-11-11 12:59:57 -0500

    Postgres container and first db migration

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM python:3.6-onbuild
+COPY migrations migrations
 CMD ["python", "-m", "flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
- 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
- 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<user %r>' % self.email
+
 @app.route('/')
 def index():
   return 'index'
